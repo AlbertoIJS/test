06f38005ad5225d4130695f7f7bd7fd26cf06530
= <=>
2020-09-12 17:40:51 -0500

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from "react";
+import './App.css';
 
-import "./App.css";
-
-import Header from "./components/header/header.component";
-import Hero from "./components/hero/hero.component";
-import Cta from "./components/cta/cta.component";
-import Footer from "./components/footer/footer.component";
-import Details from "./components/details/details.component";
+import Header from './components/header/header.component';
+import Hero from './components/hero/hero.component';
+import Cta from './components/cta/cta.component';
+import Footer from './components/footer/footer.component';
+import Details from './components/details/details.component';
 
 const App = () => (
   <>
